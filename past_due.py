21856d987482ed367feed909d82209ed2afb2122
Joseph Cecil <jcecil@isi.edu>
2022-01-08 13:47:44 -0500

    Sanity check focus roll results

--- past_due.py
+++ past_due.py
@@ -93,6 +93,10 @@ class ResolvedFocusRoll:
     reroll_counts: Sequence[int]
     phenomenality: int
 
+    def __post_init__(self):
+        if any((roll % SIDES) == 2 for roll in self.rolls):
+            raise ValueError(f"A resolved focus roll cannot include any twos!")
+
 
 def calculate_focus_roll(rolls: Sequence[int], attributes: str) -> ResolvedFocusRoll:
     """
